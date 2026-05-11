a8a325e454f9ddb7dbb139fbdd00db9493c72cbf
hafizillokomilov71-dev <hafizillokomilov71@gmail.com>
2026-05-11 06:48:09 +0500

    the and

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/home";
-import SignUp from "../pages/SignUp";
 import SignIn from "../pages/signIn";
 import NotFount from "../pages/not-fount";
+import SignUp from "../pages/signUp";
 
 export const routes = createBrowserRouter([
   { path: "/", element: <Home /> },
-  { path: "sign-up", element: <SignUp /> },
+  { path: "sign-up", element: <SignUp/> },
   { path: "sign-in", element: <SignIn /> },
   { path: "*" , element: <NotFount/>}
 ]);
